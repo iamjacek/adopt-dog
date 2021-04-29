1063bea38f5ed0554352acc7ec2f1c79c9aa1a52
iamjacek <jake47s@gmail.com>
2021-04-29 12:24:19 +0100

    hardcoded data to be printed

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ])
+}
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt dog"),
+    React.createElement(Pet, {
+      name: "Max",
+      animal: "dog",
+      breed: "Sausage Dog",
+    }),
+    React.createElement(Pet, {
+      name: "El",
+      animal: "Cat",
+      breed: "Leopard",
+    }),
+    React.createElement(Pet, {
+      name: "Sam",
+      animal: "Bird",
+      breed: "Pigeon",
+    })
+  )
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"))
