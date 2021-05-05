bbf2689cb5fb31c55c9c39c753958268b475ad84
iamjacek <jake47s@gmail.com>
2021-05-05 13:52:35 +0100

    eslint config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement(
