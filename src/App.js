4e03b4578ccd2584e6e3d6959844278ce29c2308
iamjacek <jake47s@gmail.com>
2021-05-19 22:34:54 +0100

    add details page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 
 import ReactDOM from 'react-dom'
-import React from 'react'
+import { StrictMode } from 'react'
 import SearchParams from "./SearchParams"
+import Details from './Details'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 const App = () => {
 
   return (
     <div>
-      <h1>Adopt Dog!</h1>
-     
-      <SearchParams />
+     <Router>
+      <header>
+        <Link to="/">
+          <h1>Adopt Dog!</h1>
+        </Link>
+      </header>
+       <Switch>
+        <Route path="/details/:id">
+            <Details />
+        </Route>
+        <Route path="/">
+            <SearchParams />
+        </Route>
+       </Switch>
+
+     </Router>
     </div>
     
   )
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>, 
+  document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Pet({ name, animal, breed, images, location, id }) {
   let hero = `http://pet-images.dev-apis.com/pets.none.jpg`;
   if (images.length) {
     hero = images[0];
   }
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -12,6 +14,6 @@ export default function Pet({ name, animal, breed, images, location, id }) {
         <h1>{name}</h1>
         <h2>{`${animal} — ${breed} — ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 }
